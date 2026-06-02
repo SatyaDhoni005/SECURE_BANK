8129ecc2dcf9fd4f85ea10bfd8ff6f0ff16b68b8
SatyaDhoni005 <satyadhonibenda@gmail.com>
2026-06-02 18:20:30 +0530

    Fix CSRF trusted origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
 from pathlib import Path
 from datetime import timedelta
 import os
@@ -191,3 +192,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://secure-bank-backend-j3f3.onrender.com",
+]
